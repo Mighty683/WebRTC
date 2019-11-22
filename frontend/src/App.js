a38b32b4f3391772dd3855b4918baa89e91cc6f4
BL4VAL <BL4VAL@gmail.com>
2019-11-22 17:10:56 +0100

    client update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,25 @@ import React, { useState } from 'react';
 import Bubble from './components/MainBubble';
 import Hello from './components/Hello';
 import {
-  connect
+  connect,
+  pcs
 } from './helper/socket';
 import './App.css';
+
+
 
 function App() {
   const [userName, setUserName] = useState('');
   const [users, setUsers] = useState([]);
 
+  const onUpdate = newPeer => {
+    console.log(newPeer);
+    const answerSessionDescription = await pc.createAnswer();
+    pc.setRemoteDescription(new RTCSessionDescription(newPeer.sessionDescription));
+  }
+
   async function initConnection(userName) {
-    let response = await connect(userName)
+    let response = await connect(userName, "room1")
     setUserName(userName)
     setUsers(Object.keys(response).map(key => ({
       id: key,
--- frontend/src/helper/socket.js
+++ frontend/src/helper/socket.js
@@ -1,18 +1,53 @@
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
+export const pcs = {};
 
-export function connect (userName) {
+export function connect(userName, roomName) {
   return new Promise(async (resolve, reject) => {
-    let socket= io('http://localhost:8080');
-    let pc = new RTCPeerConnection(null);
-    let sessionDescription = await pc.createOffer()
-    pc.setLocalDescription(sessionDescription);
+    let socket = io("http://localhost:8080");
 
-    socket.on('joined', resolve)
-    socket.emit('join_room', {
+    socket.on("joined", resolve);
+
+    socket.on("on_join", async joiningUsername => {
+      const pc = new RTCPeerConnection(null);
+      let offer = await pc.createOffer();
+      pc.setLocalDescription(new RTCSessionDescription(offer));
+      pcs[joiningUsername] = pc;
+      socket.emit("send_offer", {
+        roomName,
+        offer,
+        name: userName,
+        target: joiningUsername
+      });
+    });
+
+    socket.on("on_offer", async ({ offer, name, target }) => {
+      if (target === userName) {
+        const pc = new RTCPeerConnection(null);
+        let answer = await pc.createAnswer();
+        pc.setLocalDescription(new RTCSessionDescription(answer));
+        pc.setRemoteDescription(offer);
+        pcs[name] = pc;
+        socket.emit("send_answer", {
+          answer,
+          roomName,
+          target: name,
+          name: target
+        });
+      }
+    });
+
+    socket.on("on_answer", async ({ answer, name, target }) => {
+      if (target === userName) {
+        pc = pcs[name];
+        pc.setRemoteDescription(new RTCSessionDescription(answer));
+      }
+    });
+
+    socket.emit("join_room", {
       userName,
-      name: 'room1',
+      name: roomName,
       sessionDescription
-    })
+    });
   });
 }
