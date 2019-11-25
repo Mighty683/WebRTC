34485f77f12a63d9d5af7baa22d79781c996dc5c
Tomasz Szarek <tomaszszarek@appjobs.com>
2019-11-25 18:29:24 +0100

    Cleanup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import '@opentok/client';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App></App>, document.getElementById('root'));
